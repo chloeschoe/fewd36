439dfb78d2f0ff2c56a3e71584ea312838439f6a
chloeschoe <chloesjchoe@gmail.com>
2015-11-11 21:21:22 -0800

    todo
    restyled and added correct js functions

--- todo-list/todo-list.js
+++ todo-list/todo-list.js
@@ -1,53 +1,56 @@
 // Structure
 
-var form     = document.querySelector("form");
-var button   = document.querySelector("button");
-var task     = document.querySelector(".task");
-var date  	 = document.querySelector(".date");
-var list     = document.querySelector("ul");
-
-
-
-console.log(task);
-console.log(date);
+var form    = document.querySelector("form");
+var button  = document.querySelector("button");
+var date    = document.querySelector(".date");
+var task    = document.querySelector(".task");
+var list    = document.querySelector("ul");
+var checkbox= document.querySelector(".checkbox");
+ 
+console.log (task);
+console.log (date);
 
 //Events
 
-button.addEventListener("click",showTask);
-task.addEventListener("submit", addTask);
- 
+button.addEventListener ("click",addTask);
+form.addEventListener   ("submit", addTask);
+task.addEventListener   ("submit", addTask);
 
-//Setup
-var tasks = [];
 
 //Event handler function
 
 function addTask(e){
 
 	e.preventDefault();
-	tasks.push(task.value);
+	var dateValue = date.value;
+	var taskValue = task.value;
 
-	// Clean up
-	task.reset();
+
+	//Step 1 -create the element
+	var checkbox = document.createElement ("input");
+	var newTask  = document.createElement("li");
+	var span     = document.createElement("span");
+
+
+	//Step 2 -decorate the elements
+
+	checkbox.setAttribute ("type", "checkbox");	
+	span.textContent = task.value +"("+ date.value+ ")";
+
+	//Step 3 -insert the element
+	
+	list.appendChild(newTask);
+	newTask.appendChild(checkbox);
+	newTask.appendChild(span);
+	//label.appendChild(li);
+
 }
 
-function showTask(e) {
-	console.log("showTask");
-	tasks.forEach(directions);
-}
+function testIt(event) {
 
-// Update page functions
-// ---------------------------------
-function tasks(item) {
-
-	// Step 1 - create element
-	var li = document.createElement("li");
-
-	// Step 2 - blingification of the element
-	li.textContent = item;
-
-	// Step 3 - surgical insertion of our element
-	list.appendChild(li);
+	console.log("testIt");
+	console.log(event.target);
+	event.target.className ="checked";
 
 }
 
